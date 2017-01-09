3b0ee37c1fd60bd80dfca51e09d21659870934a7
Gabriel <g.lackermeier@gmx.de>
2017-01-09 21:14:28 +0100

    merged directory

--- src/klima/Klima.java
+++ src/klima/Klima.java
@@ -1,98 +1,213 @@
 package klima;
 
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import java.awt.*;
+import java.util.Scanner;
 
 public class Klima {
+
+    private Scanner scanner = new Scanner(System.in);
+    private boolean onoff = false;
+    private String tempDigit1 = " __ ", tempDigit2 = "|  |", tempDigit3 = "|__|", tempDigit4 = " __ ", tempDigit5 = "|  |", tempDigit6 = "|__|";
+    private String stufe1 = "    ", stufe2 = "   |", stufe3 = "   |";
+
+
     public static void main(String[] args) {
-        new KlimaFrame().setVisible(true);
+        new Klima().display();
 
     }
 
+    private void switchPower() {
+        onoff = !onoff;
+    }
 
-    private static class KlimaFrame extends JFrame {
-        Auto auto = new Auto();
-        JPanel panel = new JPanel();
-        JToggleButton onoff = new JToggleButton();
-        JSlider[] sliders = new JSlider[3];
+    private void setTemp(int temp) {
+        if (temp <= 15) {
+            tempDigit1 = "    ";
+            tempDigit2 = "   |";
+            tempDigit3 = "   |";
 
+            tempDigit4 = " __ ";
+            tempDigit5 = "|__ ";
+            tempDigit6 = " __|";
 
+        } else if (temp >= 25) {
+            tempDigit1 = " __ ";
+            tempDigit2 = " __|";
+            tempDigit3 = "|__ ";
 
-        KlimaFrame() {
-            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            ChangeListener1 changeListener = new ChangeListener1();
-            panel.setLayout(new GridLayout(4, 1));
-            panel.add(onoff);
-            for (JSlider js: sliders){
-                js = new JSlider();
-                js.setPaintLabels(true);
-                js.setPaintTicks(true);
-                js.setMinorTickSpacing(1);
-                js.addChangeListener(changeListener);
+            tempDigit4 = " __ ";
+            tempDigit5 = "|__ ";
+            tempDigit6 = " __|";
+        }
+        switch (temp) {
+            case 16:
+                tempDigit1 = "    ";
+                tempDigit2 = "   |";
+                tempDigit3 = "   |";
 
-            }
-            sliders[0].setMinimum(10);
-            sliders[0].setMaximum(35);
-            sliders[0].setValue((int) auto.getTemp());
+                tempDigit4 = " __ ";
+                tempDigit5 = "|__ ";
+                tempDigit6 = "|__|";
+                break;
+            case 17:
+                tempDigit1 = "    ";
+                tempDigit2 = "   |";
+                tempDigit3 = "   |";
 
+                tempDigit4 = " __ ";
+                tempDigit5 = "   |";
+                tempDigit6 = "   |";
+                break;
+            case 18:
+                tempDigit1 = "    ";
+                tempDigit2 = "   |";
+                tempDigit3 = "   |";
 
-            for (JSlider jSlider: sliders){
-                panel.add(jSlider);
-            }
+                tempDigit4 = " __ ";
+                tempDigit5 = "|__|";
+                tempDigit6 = "|__|";
+                break;
+            case 19:
+                tempDigit1 = "    ";
+                tempDigit2 = "   |";
+                tempDigit3 = "   |";
 
-            this.getContentPane().add(panel);
-            pack();
+                tempDigit4 = " __ ";
+                tempDigit5 = "|__|";
+                tempDigit6 = " __|";
+                break;
+            case 20:
+                tempDigit1 = " __ ";
+                tempDigit2 = " __|";
+                tempDigit3 = "|__ ";
 
+                tempDigit4 = " __ ";
+                tempDigit5 = "|  |";
+                tempDigit6 = "|__|";
+                break;
+            case 21:
+                tempDigit1 = " __ ";
+                tempDigit2 = " __|";
+                tempDigit3 = "|__ ";
 
+                tempDigit4 = "    ";
+                tempDigit5 = "   |";
+                tempDigit6 = "   |";
+                break;
+            case 22:
+                tempDigit1 = " __ ";
+                tempDigit2 = " __|";
+                tempDigit3 = "|__ ";
+
+                tempDigit4 = " __ ";
+                tempDigit5 = " __|";
+                tempDigit6 = "|__ ";
+                break;
+            case 23:
+                tempDigit1 = " __ ";
+                tempDigit2 = " __|";
+                tempDigit3 = "|__ ";
+
+                tempDigit4 = " __ ";
+                tempDigit5 = " __|";
+                tempDigit6 = " __|";
+                break;
+            case 24:
+                tempDigit1 = " __ ";
+                tempDigit2 = " __|";
+                tempDigit3 = "|__ ";
+
+                tempDigit4 = "    ";
+                tempDigit5 = "|__|";
+                tempDigit6 = "   |";
+                break;
         }
 
+    }
 
-        private class ChangeListener1 implements javax.swing.event.ChangeListener {
+    private void setStufe(int stufe) {
+        if (stufe <= 1) {
+            stufe1 = "    ";
+            stufe2 = "   |";
+            stufe3 = "   |";
+        } else if (stufe >= 4) {
+            stufe1 = "    ";
+            stufe2 = "|__|";
+            stufe3 = "   |";
+        }
+        switch (stufe) {
+            case 2:
+                stufe1 = " __ ";
+                stufe2 = " __|";
+                stufe3 = "|__ ";
+                break;
+            case 3:
+                stufe1 = " __ ";
+                stufe2 = " __|";
+                stufe3 = " __|";
+                break;
 
-            @Override
-            public void stateChanged(ChangeEvent e) {
-
-            }
         }
     }
 
-    private static class Auto {
-        boolean power = false;
-        float temp = 21.0f, fan;
-        char fanLoc;
+    private void display() {
+        System.out.print('\u000c');
 
+        String onoffString;
+        onoffString = onoff ? "EIN" : "AUS";
+        if (!onoff) {
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            System.out.println("|               Klima:                     |                       " + onoffString + "                  |");
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            System.out.println("|  Aktion: (p = einschalten, e = beenden)                                               |");
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            switch (scanner.next()) {
+                case "p":
+                    switchPower();
+                    display();
+                    break;
+                case "e":
+                    System.exit(0);
+                    break;
+                default:
+                    display();
+                    break;
+            }
 
-        void switchPower() {
-            power = !power;
+        } else {
+
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            System.out.println("|               Klima:                     |                       " + onoffString + "                  |");
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            System.out.println("|                                          |                                            |");
+            System.out.println("|             " + tempDigit1 + "  " + tempDigit4 + "                   |                       " + stufe1 + "                 |");
+            System.out.println("|             " + tempDigit2 + "  " + tempDigit5 + "                   |                       " + stufe2 + "                 |");
+            System.out.println("|             " + tempDigit3 + "  " + tempDigit6 + "                   |                       " + stufe3 + "                 |");
+            System.out.println("|                                          |                                            |");
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            System.out.println("|  Aktion: (p = ausschalten, t = Temperatur, s = Stufe                                  |");
+            System.out.println("+------------------------------------------+--------------------------------------------+");
+            switch (scanner.next()) {
+                case "p":
+                    switchPower();
+                    display();
+                    break;
+                case "t":
+                    System.out.println("neue Temperatur: ");
+                    setTemp(scanner.nextInt());
+                    display();
+                    break;
+                case "s":
+                    System.out.println("neue Stufe: ");
+                    setStufe(scanner.nextInt());
+                    display();
+                    break;
+                default:
+                    display();
+                    break;
+            }
         }
 
-        void setTemp(float temp) {
-            this.temp = temp;
-        }
-
-        void setFan(float fan) {
-            this.fan = fan;
-        }
-
-        void setFanLoc(char location) {
-            fanLoc = location;
-
-        }
-
-        public float getTemp() {
-            return temp;
-        }
-
-        public float getFan() {
-            return fan;
-        }
-
-        public char getFanLoc() {
-            return fanLoc;
-        }
     }
-
 
 }
