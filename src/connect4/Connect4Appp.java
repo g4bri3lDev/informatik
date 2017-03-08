38ddb48142758900295782de55dc4228fa69bb30
Gabriel <g.lackermeier@gmx.de>
2017-03-08 14:24:22 +0100

    Connect 4 finished

--- src/connect4/Connect4Appp.java
+++ src/connect4/Connect4Appp.java
@@ -1,6 +1,8 @@
 package connect4;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -9,9 +11,13 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 /**
@@ -25,6 +31,8 @@ public class Connect4Appp extends Application {
     private boolean redTurn = true;
     private Disc[][] grid = new Disc[COLUMNS][ROWS];
 
+    private Pane discPane = new Pane();
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -32,11 +40,14 @@ public class Connect4Appp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(new Scene(createContent()));
+        primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
     private Parent createContent() {
         Pane root = new Pane();
+        root.getChildren().add(discPane);
         Shape gridShape = genGrid();
         root.getChildren().add(gridShape);
         root.getChildren().addAll(columnList());
@@ -75,6 +86,75 @@ public class Connect4Appp extends Application {
     }
 
     private void placeDisc(Disc disc, int column) {
+        int row = ROWS - 1;
+        do {
+            if (!getDisc(column, row).isPresent()) {
+                break;
+            }
+            row--;
+        } while (row >= 0);
+        if (row < 0) {
+            return;
+        }
+        grid[column][row] = disc;
+        discPane.getChildren().add(disc);
+        disc.setTranslateX(column * (TILESIZE + 5) + TILESIZE / 4);
+        final int localRow = row;
+        TranslateTransition animation = new TranslateTransition(Duration.seconds(1), disc);
+        animation.setToY(row * (TILESIZE + 5) + TILESIZE / 4);
+        animation.setOnFinished(e -> {
+            if (gameEnd(column, localRow)) {
+                gameFinish();
+            }
+            redTurn = !redTurn;
+        });
+        animation.play();
+    }
+
+    private boolean check4(List<Point2D> point2DList) {
+        int combination = 0;
+        for (Point2D p : point2DList) {
+            int col = (int) p.getX();
+            int row = (int) p.getY();
+            Disc disc = getDisc(col, row).orElse(new Disc(!redTurn));
+            if (disc.red == redTurn) {
+                combination++;
+                if (combination == 4) {
+                    return true;
+                }
+
+            } else {
+                combination = 0;
+            }
+        }
+        return false;
+    }
+
+    private boolean gameEnd(int col, int row) {
+
+        List<Point2D> vertical = IntStream.rangeClosed(row - 3, row + 3).mapToObj(r -> new Point2D(col, r)).collect(Collectors.toList());
+        List<Point2D> horizontal = IntStream.rangeClosed(col - 3, col + 3).mapToObj(c -> new Point2D(c, row)).collect(Collectors.toList());
+
+        Point2D topL = new Point2D(col - 3, row - 3);
+        List<Point2D> diagonalLeft = IntStream.rangeClosed(0, 6).mapToObj(i -> topL.add(i, i)).collect(Collectors.toList());
+
+        Point2D bottL = new Point2D(col - 3, row + 3);
+        List<Point2D> diagonalRight = IntStream.rangeClosed(0, 6).mapToObj(i -> bottL.add(i, -i)).collect(Collectors.toList());
+
+        return check4(vertical) || check4(horizontal) || check4(diagonalLeft) || check4(diagonalRight);
+    }
+
+
+    private void gameFinish() {
+        System.out.println("Winner: " + (redTurn ? "RED" : "YELLOW"));
+        System.exit(0);
+    }
+
+    private Optional<Disc> getDisc(int col, int row) {
+        if (col < 0 || col >= COLUMNS || row < 0 || row >= ROWS) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(grid[col][row]);
 
     }
 
